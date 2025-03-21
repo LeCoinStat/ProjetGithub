543ed4443d42f9669c05accad9d92c83bb8f4c3b
LeCoinStat <128073334+LeCoinStat@users.noreply.github.com>
2025-03-21 20:22:15 +0100

    Ajout d'un code python
    Ajout d'un code python

---
+++ def elevé_au_carré(nombre):.py
@@ -0,0 +1,6 @@
+def elevé_au_carré(nombre):
+   return nombre ** 2
+
+# Test de la fonction avec différents nombres
+print(elevé_au_carré(2))  # doit afficher 4
+print(elevé_au_carré(-3))  # doit afficher 9
